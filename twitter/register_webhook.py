4603589f1a0e904af76845a13ef6b4a464438b92
Dennis Peterson <denispeterson96@gmail.com>
2019-10-01 13:31:31 +0300

    now able to reply on a status

--- twitter/register_webhook.py
+++ twitter/register_webhook.py
@@ -3,6 +3,7 @@ import urllib
 from django.conf import settings
 import pprint
 
+
 class Twitter_Activities:
     CONSUMER_KEY = settings.CONSUMER_KEY
     CONSUMER_SECRET = settings.CONSUMER_SECRET
@@ -12,15 +13,17 @@ class Twitter_Activities:
                             client_secret=CONSUMER_SECRET,
                             resource_owner_key=ACCESS_TOKEN,
                             resource_owner_secret=ACCESS_SECRET)
+
     def register_crc(self):
-        webhook_endpoint = urllib.parse.quote_plus('https://44c3b65c.ngrok.io/api/crc_callback')
+        webhook_endpoint = urllib.parse.quote_plus(
+            'https://9e539681.ngrok.io/api/crc_callback')
         url = 'https://api.twitter.com/1.1/account_activity/all/notifier/webhooks.json?url={}'.format(
             webhook_endpoint)
         return self.twitter.post(url)
 
     def active_webhooks(self):
         url = 'https://api.twitter.com/1.1/account_activity/all/webhooks.json'
-        headers = {'Authorization': 'Bearer %s' % self.ACCESS_TOKEN }
+        headers = {'Authorization': 'Bearer %s' % self.ACCESS_TOKEN}
         res = self.twitter.get(url, headers=headers)
         one = res.json()['environments']
         two = one[0]
@@ -29,40 +32,46 @@ class Twitter_Activities:
 
         return res.json()
 
-    # Delete webhoook and create another whenever you change callback url. 
+    # Delete webhoook and create another whenever you change callback url.
     # Reregistering callback on an already existing callback doesnt work.
-    # def delete_webhook(self, webhook_id):
-    #     url = 'https://api.twitter.com/1.1/account_activity/all/notifier/webhooks/1174811182877892608.json'
-    #     headers = {
-    #         'authorization': 'OAuth oauth_consumer_key="%s", oauth_token="%s"' % (self.CONSUMER_KEY, self.ACCESS_TOKEN)
-    #     }
-    #     res = self.twitter.delete(url, headers=headers)
-    #     return res
+    def delete_webhook(self):
+        url = 'https://api.twitter.com/1.1/account_activity/all/notifier/webhooks/1176139247960633344.json'
+        headers = {
+            'authorization': 'OAuth oauth_consumer_key="%s", oauth_token="%s"' % (self.CONSUMER_KEY, self.ACCESS_TOKEN)
+        }
 
-    def delete_reply(self, body):
-       tweet_create_events = body['tweet_create_events']
-       tweet_create_events = tweet_create_events[0]
-       tweet_id = tweet_create_events['id_str']
-       initial_tweeter = tweet_create_events['in_reply_to_status_id']
-       reply_tweet = tweet_create_events['text']
+        return self.twitter.delete(url, headers=headers)
 
-       url = 'https://api.twitter.com/1.1/statuses/destroy/{}.json'.format(tweet_id)
-       pprint.pprint(self.twitter)
-       deleted_tweet = self.twitter.post(url)
-       print('------------------deleted tweet----------------------')
-       pprint.pprint(deleted_tweet.json())
-       return deleted_tweet
+    def delete_reply(self, content):
+        tweet_body = content[0]
+        tweet_id = tweet_body['id_str']
+        in_reply_to_screen_name = tweet_body['in_reply_to_screen_name']
+        in_reply_to_status_id = tweet_body['in_reply_to_status_id']
+        reply_text = tweet_body['text'] + ':smiley:'
+        delete_tweet_url = 'https://api.twitter.com/1.1/statuses/destroy/{}.json'.format(
+            tweet_id)
 
-    
+        print('------------------deleting tweet----------------------')
+        self.twitter.post(delete_tweet_url)
 
-    def deleted_tweet(self, tweet):
-        return tweet
+        return [in_reply_to_screen_name, in_reply_to_status_id, reply_text]
 
+    def deleted_tweet(self, content):
+        return content
+
+    def tweet_reply(self, user_name, in_reply_to_status_id, message):
+        tweet_reply_url = 'https://api.twitter.com/1.1/statuses/update.json'
+        params = {
+            'status': message,
+            'in_reply_to_screen_name': user_name,
+            'in_reply_to_status_id': in_reply_to_status_id
+        }
+        print('------------------making a reply----------------------')
+        return self.twitter.post(tweet_reply_url, params=params)
 
     def adding_subscription(self):
         url = 'https://api.twitter.com/1.1/account_activity/all/notifier/subscriptions.json'
         headers = {
             'authorization': 'OAuth oauth_consumer_key="%s", oauth_token="%s"' % (self.CONSUMER_KEY, self.ACCESS_TOKEN)
         }
-        res = self.twitter.post(url, headers=headers)
-        return res
+        return self.twitter.post(url, headers=headers)
--- twitter/views.py
+++ twitter/views.py
@@ -9,16 +9,18 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from twitter.register_webhook import Twitter_Activities
 
+
 def index(request):
     activity = Twitter_Activities()
     activity.register_crc()
-    # r = activity.adding_subscription()
-    res = activity.active_webhooks()
-    # response = activity.delete_webhook(res)
+    response = activity.delete_webhook()
     # print(response.text)
+    r = activity.adding_subscription()
     # print(r.json())
+    res = activity.active_webhooks()
     print(res)
     return render(request, 'index.html', {})
+
 
 @csrf_exempt
 def process_crc_callback(request):
@@ -34,21 +36,33 @@ def process_crc_callback(request):
             'response_token': 'sha256=' + base64.b64encode(sha256_hash_digest).decode('utf-8')
         }
         return HttpResponse(json.dumps(response))
-        
+
     else:
-        print("--------------------------------------------------------")
+        print('-------------------processing callback--------------------')
         tweet_event_body = json.loads(request.body)
-        pprint.pprint(tweet_event_body)
-        if 'tweet_create_events' in tweet_event_body.keys():
-            twitter.delete_reply(tweet_event_body)
+        events = tweet_event_body.keys()
+        if 'tweet_create_events' in events and tweet_event_body['tweet_create_events'][0]['in_reply_to_status_id']:
+            content = tweet_event_body['tweet_create_events']
+            [in_reply_to_screen_name, in_reply_to_status_id,
+                reply_text] = twitter.delete_reply(content)  # small listening to events
 
-        elif 'tweet_delete_events' in :
+            res = twitter.tweet_reply(
+                in_reply_to_screen_name, in_reply_to_status_id, reply_text)
+            print('-------------tweet reply response-------------------------')
+            pprint.pprint(res.json())
+
+        elif 'tweet_delete_events' in events:
             print('---------------the deleted tweet was----------------')
+            content = tweet_event_body['tweet_delete_events']
+            print(tweet_event_body)
+            twitter.deleted_tweet(content)
+        else:
+            print('in last else')
+            print(tweet_event_body)
 
-        
         return HttpResponse(tweet_event_body)
+
 
 def denno_api(request):
     response = request.POST
     print(response)
-    
